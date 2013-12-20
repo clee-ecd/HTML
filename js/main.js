073cfffbf64e28210b8f4c9e3a8e5aba5e130f37
clee-ecd <clee@ecdgraphics.ca>
2013-12-20 15:08:40 -0800

    First batch of updates - grid view setup

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// JavaScript Document
+
+
+$(document).ready(function(){
+/*
+	var browserheight = $(window).height();
+	$(".homeslider").css("height",browserheight);
+	//console.log;
+	
+	
+	$("#mobilemmenu").click(function(){
+		//slide up all the link lists
+		$("ul#menu-main").slideToggle();
+		
+	});
+	
+	
+	$("#menu-main .menu-item-has-children > a").click(function(){
+		//slide up all the link lists
+		$(this).siblings("ul").slideDown();
+		$(this).parent().siblings().children("ul").slideUp();
+		
+		//$("#menu-main .menu-item-has-children ul").slideToggle();
+		if(!$(this).next().is(":visible"))
+		{
+			$(this).next().slideDown();
+		}
+	});
+	
+	
+	
+	$('.bxslider').bxSlider({
+		 pagerCustom: '#bx-pager',
+		 auto:true
+		 //autoControls: true
+	});
+
+*/
+	
+	
+	}); //end ready
+	
+	
+	
+	/*
+
+$(window).resize(function() {
+    var browserheight = $(window).height();
+	
+	$(".homeslider").css("height",browserheight);
+});
+
+
+
+
+
+ window.addEventListener("load",function() {
+   setTimeout(function(){
+    window.scrollTo(0, 0);
+    }, 0);
+ });
+ 
+ */
+
