708667df695028e7e1c7b5a97e301405c7dd7ab2
clee-ecd <clee@ecdgraphics.ca>
2013-12-31 14:27:19 -0800

    Added English Section & Finalized Chinese

--- js/main.js
+++ js/main.js
@@ -1,64 +1,12 @@
 // JavaScript Document
+function getParameterByName(name) {
+		var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+		return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+	}
 
+var id = getParameterByName("id");
 
-$(document).ready(function(){
-/*
-	var browserheight = $(window).height();
-	$(".homeslider").css("height",browserheight);
-	//console.log;
-	
-	
-	$("#mobilemmenu").click(function(){
-		//slide up all the link lists
-		$("ul#menu-main").slideToggle();
-		
-	});
-	
-	
-	$("#menu-main .menu-item-has-children > a").click(function(){
-		//slide up all the link lists
-		$(this).siblings("ul").slideDown();
-		$(this).parent().siblings().children("ul").slideUp();
-		
-		//$("#menu-main .menu-item-has-children ul").slideToggle();
-		if(!$(this).next().is(":visible"))
-		{
-			$(this).next().slideDown();
-		}
-	});
 	
 	
 	
-	$('.bxslider').bxSlider({
-		 pagerCustom: '#bx-pager',
-		 auto:true
-		 //autoControls: true
-	});
-
-*/
 	
-	
-	}); //end ready
-	
-	
-	
-	/*
-
-$(window).resize(function() {
-    var browserheight = $(window).height();
-	
-	$(".homeslider").css("height",browserheight);
-});
-
-
-
-
-
- window.addEventListener("load",function() {
-   setTimeout(function(){
-    window.scrollTo(0, 0);
-    }, 0);
- });
- 
- */
-
